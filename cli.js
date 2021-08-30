92e1c8b15e1ca821a4e52b997d7e13870a193c3a
galymax <werkhowski@gmail.com>
2021-08-31 00:05:23 +0200

    bash copy

--- cli.js
+++ cli.js
@@ -14,7 +14,7 @@ console.log('Setup autograding');
 function insertTemplateFiles() {
   console.log('Inserting autograding files');
   //fse.copySync(templateDir, root);
-  exec(`rsync -a ${templateDir} ${root}`);
+  exec(`shopt -s dotglob && rsync -a ${templateDir}/. ${root}`)
 }
 
 insertTemplateFiles();
