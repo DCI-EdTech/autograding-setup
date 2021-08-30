9a3b92dc859eaa30840fc891edc36fd34595c162
galymax <werkhowski@gmail.com>
2021-08-30 20:48:49 +0200

    remove install

--- cli.js
+++ cli.js
@@ -9,15 +9,11 @@ const templateDir = path.resolve(orig, 'template');
 
 console.log('Setup autograding');
 
-console.log('root', root);
-console.log('orig', orig);
-
-// install dependencies
-exec('npm install');
-
 function insertTemplateFiles() {
-  console.log('Inserting template files');
+  console.log('Inserting autograding files');
   fse.copySync(templateDir, root);
 }
 
 insertTemplateFiles();
+
+process.exit();
