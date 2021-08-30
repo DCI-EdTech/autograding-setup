6be6c4bf5dc60f92a067126974a7eecce66ab56d
galymax <werkhowski@gmail.com>
2021-08-30 21:04:33 +0200

    add package json scriüpts

--- cli.js
+++ cli.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 const {exec} = require('child_process');
+const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
 
 const root = path.resolve('./');
 const orig = __dirname;
 const templateDir = path.resolve(orig, 'template');
+const packageJson = require(path.resolve(root, 'package.json'));
 
 console.log('Setup autograding');
 
@@ -16,5 +18,10 @@ function insertTemplateFiles() {
 
 insertTemplateFiles();
 
-console.log('done')
+Object.assign(packageJson.scripts, {
+  "test": "jest",
+  "test:watch": "jest --watch"
+});
+
+console.log('autograding pre-setup done')
 process.exit();
