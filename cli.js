ecc36ad6e26ee9b62fa258213bfd077979ad3fa8
galymax <werkhowski@gmail.com>
2021-08-30 17:55:20 +0200

    testing

--- cli.js
+++ cli.js
@@ -1,2 +1,21 @@
 #!/usr/bin/env node
-console.log('Setup autograding')
+const {exec} = require('child_process');
+const fse = require('fs-extra');
+const path = require('path');
+
+console.log('Setup autograding');
+
+const root = path.resolve('./');
+console.log('root', root);
+
+// install dependencies
+exec('npm install');
+
+// copy files
+fse.copySync('./template/testfile', root, { overwrite: true }, function (err) {
+  if (err) {                 
+    console.error(err);
+  } else {
+    console.log("copy success!");
+  }
+});
