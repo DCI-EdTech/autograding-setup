20ef68882cd0f2ef232623231e9cd66d38817a4e
galymax <werkhowski@gmail.com>
2021-09-06 09:18:47 +0200

    remove logs

--- scripts/modifyReadme.js
+++ scripts/modifyReadme.js
@@ -8,11 +8,9 @@ exports.modifyReadme = function (readmePath, readmeInfoPath) {
   const readmeInfo = fse.readFileSync(readmeInfoPath, 'utf8');
   let readme = fse.readFileSync(readmePath, 'utf8')
   const re = new RegExp(`[\n\r]*${escapeRegExp(delimiters[0])}([\\s\\S]*)${escapeRegExp(delimiters[1])}`, 'gsm');
-  console.log('re', re)
   // remove badge line
   readme = readme.replace(/\!\[Points badge\]\(.*[\n\r]*/g, '')
   // remove autograding info
-  console.log(readme.match(re))
   readme = readme.replace(re, '')
   // insert badge line and autograding info
   fse.writeFileSync(readmePath, `${pointsBadgeString}${readme}\n\r${delimiters[0]}\n${readmeInfo}\n\r${delimiters[1]}`);
