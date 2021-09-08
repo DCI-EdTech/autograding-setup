e24c6f18b84177d486136879185a3354135a4034
galymax <werkhowski@gmail.com>
2021-09-08 08:15:26 +0200

    gitignore package-lock in dev mode

--- scripts/insertTemplateFiles.js
+++ scripts/insertTemplateFiles.js
@@ -7,6 +7,6 @@ exports.insertTemplateFiles = function(templateDir, ignoreList, outputPath) {
   console.log('Inserting autograding files');
   if(!devMode) fse.copySync(templateDir, root);
   // .gitignore needs to be generated because of a bug in npm v7 https://github.com/npm/cli/issues/2144
-  if(devMode) ignoreList.push('.gitignore')
+  if(devMode) ignoreList.push('.gitignore', 'package-lock.json')
   fse.writeFileSync(outputPath, ignoreList.join('\n'));
 }
