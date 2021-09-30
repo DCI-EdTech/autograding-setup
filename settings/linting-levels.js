4c9390b2aadf93451aa818405b5f60cc30ede016
galymax <werkhowski@gmail.com>
2021-10-01 01:10:55 +0200

    linting low level single quotes off

--- settings/linting-levels.js
+++ settings/linting-levels.js
@@ -4,7 +4,8 @@ module.exports = {
     'eol-last': 'off',
     'semi': 'off',
     'prefer-template': 'off',
-    'no-unused-vars': 'off'
+    'no-unused-vars': 'off',
+    'quotes': 'off' // don't enforce single quotes
   },
   'medium': {
     'no-console': 'off',
