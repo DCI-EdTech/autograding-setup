e70863e45b653eebe0174208f757ecd859aff633
galymax <werkhowski@gmail.com>
2021-10-08 11:40:14 +0200

    changed points calculation

--- scripts/generateAutogradingJSON.js
+++ scripts/generateAutogradingJSON.js
@@ -3,19 +3,21 @@ const path = require('path');
 
 exports.generateAutogradingJSON = function(testsDir, outputPath) {
   // read test folder contents  
-  const testFiles = fse.readdirSync(testsDir);
-  // filer autograding test files
-  const autogradingTests = testFiles.reduce((acc, file, i, list) => {
+  const testFiles = fse.readdirSync(testsDir).reduce((acc, file) => {
     const taskName = path.basename(file).match(/^tasks\.(.*)\.js$/)[1];
-    if(taskName) acc.push({
-      "name": `Task ${taskName}`,
+    if(taskName) acc.push({taskName, file});
+  }, []);
+  // filer autograding test files
+  const autogradingTests = testFiles.map((item, i, list) => {
+    const pointsPerTask = Math.round(100/list.length)
+    return {
+      "name": `Task ${item.taskName}`,
       "setup": "npm install --ignore-scripts",
-      "run": `npm test -- ${testsDir}/${file}`,
+      "run": `npm test -- ${testsDir}/${item.file}`,
       "timeout": 10,
-      "points": 100/list.length
-    })
-    return acc;
-  }, [])
+      "points": i === list.length-1 ? 100 - pointsPerTask * (list.length-1) : pointsPerTask
+    }
+  })
   const autogradingJSON = {
     tests: autogradingTests
   };
