d142ad4fb44a5f34f597433fcc8b1df35a2919b4
galymax <werkhowski@gmail.com>
2021-10-20 12:40:26 +0200

    log copied files

--- scripts/insertTemplateFiles.js
+++ scripts/insertTemplateFiles.js
@@ -5,7 +5,10 @@ const { root } = require('../lib/refs')
 
 exports.insertTemplateFiles = function(templateDir, ignoreList, outputPath) {
   console.log('Inserting autograding files');
-  if(!devMode) fse.copySync(templateDir, root);
+  if(!devMode) fse.copySync(templateDir, root, { filter: (src, dest) => {
+    console.log(src, dest)
+    return true
+  }});
   // .gitignore needs to be generated because of a bug in npm v7 https://github.com/npm/cli/issues/2144
   if(devMode) ignoreList.push('.gitignore', 'package-lock.json')
   fse.writeFileSync(outputPath, ignoreList.join('\n'));
