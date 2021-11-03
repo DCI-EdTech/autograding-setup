4ed87c30bde153b8e0fc8df8bb8849ec2a5df99a
galymax <werkhowski@gmail.com>
2021-11-03 12:31:49 +0100

    refactor package.json generation and add react mode

--- scripts/modifyPackageJson.js
+++ scripts/modifyPackageJson.js
@@ -3,13 +3,14 @@ const argv = require('../lib/yargs')
 const lintingLevels = require('../settings/linting-levels')
 const devMode = argv.dev;
 const lintingStringency = argv.lintingStringency
+const devDependencies = require('../settings/devDependencies')
 
 exports.modifyPackageJson = async function(packageJsonPath) {
   const packageJson = require(packageJsonPath);
   const originalPackageJson = JSON.parse(JSON.stringify(packageJson))
+  const isReact = (packageJson.dependencies.react || packageJson.devDependencies.react) && packageJson.scripts.test.includes('react-scripts');
   packageJson.scripts = Object.assign({
     "test": "jest",
-    "test:watch": "jest --watch",
     "prepare": "npm_config_yes=true npx husky install"
   }, packageJson.scripts);
 
@@ -20,26 +21,27 @@ exports.modifyPackageJson = async function(packageJsonPath) {
       "prepare": ""
     });
   }
+
+  if(!isReact) packageJson.stylelint = Object.assign({
+    "extends": "stylelint-config-standard",
+    "rules": lintingLevels[lintingStringency]['css']
+  }, packageJson.stylelint);
   
   Object.assign(packageJson, {
     "eslintIgnore": [
       "__tests__/*.js",
       "jest.config.js"
     ],
-    "stylelint": {
-      "extends": "stylelint-config-standard",
-      "rules": lintingLevels[lintingStringency]['css']
-    },
-    "lint-staged": {
+    "lint-staged": Object.assign({
       "*.{js,html}": [
         "npx eslint --cache"
-      ],
+      ]}, (!isReact && {
       "*.css": "stylelint",
       "*.scss": "stylelint --syntax=scss"
-    }
+    }))
   });
 
-  packageJson.eslintConfig = Object.assign({
+  packageJson.eslintConfig = isReact ? { "extends": "react-app" } : Object.assign({
     "plugins": ["@html-eslint"],
     "overrides": [{
       "files": ["*.html"],
@@ -63,23 +65,7 @@ exports.modifyPackageJson = async function(packageJsonPath) {
     ]
   }, packageJson.jest);
   
-  packageJson.devDependencies = Object.assign({
-    "@html-eslint/eslint-plugin": "^0.11.0",
-    "@html-eslint/parser": "^0.11.0",
-    "eslint": "^7.32.0",
-    "eslint-config-airbnb": "^18.2.1",
-    "eslint-plugin-jsx-a11y": "^6.4.1",
-    "eslint-plugin-react": "^7.26.1",
-    "eslint-plugin-react-hooks": "^4.2.0",
-    "eslint-plugin-import": "^2.25.2",
-    "eslint-plugin-node": "^11.1.0",
-    "eslint-plugin-promise": "^5.1.0",
-    "husky": "^7.0.1",
-    "jest": "^26.6.3",
-    "lint-staged": "^11.1.2",
-    "stylelint": "^13.13.1",
-    "stylelint-config-standard": "^22.0.0"
-  }, packageJson.devDependencies);
+  packageJson.devDependencies = Object.assign(isReact ? devDependencies.minimal : devDependencies.full, packageJson.devDependencies);
   
   await fse.writeFile(packageJsonPath, JSON.stringify(packageJson, null, 2));
 }
